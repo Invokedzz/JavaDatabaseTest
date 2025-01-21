869bc529d84e84ac488f4e9a6304db4e6999be76
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-21 18:26:02 -0300

    preparing stuff to access the postgresql database

---
+++ src/main/java/src/db/DbException.java
@@ -0,0 +1,16 @@
+package src.db;
+
+import java.io.Serial;
+
+public class DbException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DbException (String message) {
+
+        super( message );
+
+    }
+
+}
