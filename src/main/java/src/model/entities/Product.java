4659f685369902a5f44b5662e730c5701b3b4df5
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-22 13:09:49 -0300

    adding entities

---
+++ src/main/java/src/model/entities/Product.java
@@ -0,0 +1,59 @@
+package src.model.entities;
+
+public class Product {
+
+    private Integer id;
+
+    private String name;
+
+    private Double price;
+
+    private Category category;
+
+    public Product () {}
+
+    public Product ( Integer id, String name, Double price, Category category ) {
+
+        this.id = id;
+
+        this.name = name;
+
+        this.price = price;
+
+        this.category = category;
+
+    }
+
+    public Integer getId () {
+
+        return id;
+
+    }
+
+    public Double getPrice () {
+
+        return price;
+
+    }
+
+    public String getName () {
+
+        return name;
+
+    }
+
+    public Category getCategory () {
+
+        return category;
+
+    }
+
+    @Override
+    public String toString () {
+
+        return getId() + " " + getName() + " "
+                + getPrice() + " " + getCategory();
+
+    }
+
+}
