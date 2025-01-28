4696aa1ffc2778fda6324b1758d31f01723a30e9
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-28 11:01:19 -0300

    creating the interface

--- src/main/java/src/application/App.java
+++ src/main/java/src/application/App.java
@@ -1,4 +1,15 @@
 package src.application;
 
+import src.view.Home.Home;
+
+import javax.swing.*;
+
 public class App {
+
+    public static void main (String[] args) {
+
+        new Home();
+
+    }
+
 }
